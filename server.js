64999957c42b22a2c71e4ff4d5bbf3924c7ec830
lamsharkie <gibbs.brandon.d@gmail.com>
2020-04-14 13:18:06 -0700

    added /location route

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-
+const cors = require('cors');
+app.use(cors());
 
 //Establish the PORT
 const PORT= process.env.PORT || 3005;
@@ -13,10 +14,31 @@ const PORT= process.env.PORT || 3005;
 
 
 // Routes
-
+app.get('/location', searchLocation);
 
 
 // Callback Functions
+function searchLocation (request, response) {
+    let city = request.query.city;
+    console.log('searchData', request.query.city);
+    let geoData = require('./data/geo.json');
+
+    let location = new City(city, geoData[0]);
+
+    response.status(200).send(location);
+    console.log('locationData', location);
+}
+
+
+// Constructor Functions
+
+function City(city, obj){
+    this.search_query = city;
+    this.formatted_query = obj.display_name;
+    this.latitude = obj.lat;
+    this.longitude = obj.lon;
+}
+
 
 
 

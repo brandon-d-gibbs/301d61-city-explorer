008550f5e23051f9c193133441a502e47eb7accb
lamsharkie <gibbs.brandon.d@gmail.com>
2020-04-15 13:23:09 -0700

    location API OFFICIALLY working. for real this time.

--- server.js
+++ server.js
@@ -24,17 +24,18 @@ function searchLocation (request, response) {
     let key = process.env.GEO_CODE_API;
     const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
     console.log('city', city)
+
     superagent.get(url)
         .then(results => {
             let searchCity = results.body[0];
-            console.log('searchCity', results.body[0]);
-            let location = new City(searchCity, city);
+            console.log('searchCity', searchCity);
+            let location = new City(city, searchCity);
 
             // console.log('searchData', request.query.city);
             // let geoData = require('./data/geo.json');    
             // let location = new City(city, geoData[0]);    
-            console.log('locationData', location);
-
+            // console.log('locationData', location);
+            console.log('drilled', results.body[0]);
             response.status(200).send(location);
          })
     }
@@ -48,16 +49,16 @@ function searchLocation (request, response) {
 function getWeather (request, response) {    
     try {
     console.log('In the clouds');
+    // let key = process.env.WEATHER_API;
+    // let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+
     let weather = require('./data/darksky.json');
     let weatherArray = weather.daily.data;
     console.log('test weather', weather.daily.data);
     let newWeatherArray = weatherArray.map((day) => {
        return new Weather(day);       
     });
-    // weather.daily.data.forEach((day) => {
-    //     weatherArray.push(new Weather(day));
-    //     console.log('weaterArray', weatherArray);
-    // })
+
     response.status(200).send(newWeatherArray);
     }catch(err){
         response.status(500).send(err);

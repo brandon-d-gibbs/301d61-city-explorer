9dde76339c4f3630ed5c4142d9c904d1eb8c5b6d
lamsharkie <gibbs.brandon.d@gmail.com>
2020-04-15 12:20:49 -0700

    location API seriously working.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const superagent = require('superagent');
+const superagent = require('superagent');
 
 const cors = require('cors');
 app.use(cors());
@@ -21,19 +21,29 @@ app.get('/weather', getWeather);
 function searchLocation (request, response) {
     try{
     let city = request.query.city;
-    console.log('searchData', request.query.city);
-    let geoData = require('./data/geo.json');
-    
-    let location = new City(city, geoData[0]);
-    
-    console.log('locationData', location);
-    response.status(200).send(location);
+    let key = process.env.GEO_CODE_API;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+    console.log('city', city)
+    superagent.get(url)
+        .then(results => {
+            let searchCity = results.body[0];
+            console.log('searchCity', results.body[0]);
+            let location = new City(searchCity, city);
+
+            // console.log('searchData', request.query.city);
+            // let geoData = require('./data/geo.json');    
+            // let location = new City(city, geoData[0]);    
+            console.log('locationData', location);
+
+            response.status(200).send(location);
+         })
     }
     catch(err){
         response.status(500).send(err);
         console.log('Sorry, we messed some stuff up.');
     }
 }
+    
 
 function getWeather (request, response) {    
     try {
